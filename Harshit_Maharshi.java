0d1d84a36bebd539db9117108bd707c814828359
Harshit Maharshi <harshitmhrsi.98@gmail.com>
2018-08-16 22:37:04 +0530

    Adding changed Harshit_Maharshi.java

--- Harshit_Maharshi.java
+++ Harshit_Maharshi.java
@@ -1,24 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 class Harshit_Maharshi {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		int ch, i, n = Integer.parseInt(br.readLine());
-		String[] m = new String[n];
-		int[] j = new int[n];
+		//Reading the text
+		String text = br.readLine();
+		int n = Integer.parseInt(br.readLine());
+		String[] patterns = new String[n];
+		//Reading patterns
+		for(int i=0;i<n;i++)
+			patterns[i] = br.readLine();
+		//Function to get index for the patterns in the text
+		getIndex(text, patterns);
+	}
+	
+	static void getIndex(String t1, String[] pat) {
+		int[] j = new int[pat.length];
 		Arrays.fill(j,-1);
-		for(i=0;i<n;i++)
-			m[i] = br.readLine();
-		for(i=0;i<n;) {
+		int ch;
+		for(int i=0;i<pat.length;) {
 			ch = 0;
-			while((j[i]=str.indexOf(m[i], j[i]+1)) != -1) {
+			while((j[i]=t1.indexOf(pat[i], j[i]+1)) != -1) {
 				System.out.print(j[i]+" ");
 				ch = 1;
 				i++;
 				break;
 			}
-			i=i==n?0:i;
+			i=i==pat.length?0:i;
 			if(ch==0)
 				break;
 		}
